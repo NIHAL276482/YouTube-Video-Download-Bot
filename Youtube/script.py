21797ca30d76f29157b040aa9d46b5282bcaa0d3
NIHAL276482 <nihalchakradhri9@gmail.com>
2025-01-20 22:31:46 +0530

    Update script.py

--- Youtube/script.py
+++ Youtube/script.py
@@ -7,9 +7,9 @@ class Translation(object):
 ├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
 ├📢 **Framework** : [PyroFork 2.3.50](https://docs.pyrogram.org/)
 ├💮 **Language** : [Python 3.12.6](https://www.python.org)
-├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
-├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
-├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
-├🎓 **Developer** : [LISA 👑](https://t.me/LISA_FAN_LK)
+
+├🥏 **Channel** : []()
+├
+├🎓 **Developer** : [UNKNOWN ](https://t.me/@ilovethatbreaths) 
 ╰───────────────⍟
 """
